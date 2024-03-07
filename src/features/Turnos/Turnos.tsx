e0aa67c7fd74f8ed43feb0ade02c86cab898b621
Martin Martinez <104086277+Pipoteex@users.noreply.github.com>
2024-03-07 15:02:57 -0300

    actualizacion:retoques

--- src/features/Turnos/Turnos.tsx
+++ src/features/Turnos/Turnos.tsx
@@ -11,23 +11,23 @@ function Turnos() {
         <img
           src={misionesGreenSrc}
           alt=""
-          className=" w-[32%] absolute right-[13%] z-[1]"
+          className=" w-[29%] absolute right-[13%] z-[1]"
         />
         <img
           src={misionesRedSrc}
           alt=""
-          className=" w-[32%] absolute right-[10%] z-[2]"
+          className=" w-[29%] absolute right-[10%] z-[2]"
         />
       </div>
 
-      <div className=" relative w-[100%] h-[80%] flex items-center justify-center flex-col bg-[#003C60]">
+      <div className=" relative w-[100%] h-[100%] flex items-center justify-center flex-col bg-[#003C60]">
         <img
           src={logoSrc}
           alt=""
           className="w-[14%] absolute left-[0%] bottom-[40%] z-[1]"
         />
-        <img src={butomimageSrc} alt="" className="absolute top-0 z-[0]" />
-        <div className=" flex items-center justify-center flex-col absolute bottom-[20%]  ">
+        <img src={butomimageSrc} alt="" className="absolute top-0 z-[0] " />
+        <div className=" flex items-center justify-center flex-col absolute bottom-[23%]  ">
           <span className=" font-semibold text-[40px] leading-[68px] text-[#FFFFFF] relative ">
             ¡realizar tus tramites ahora es mucho mas facil y rapido!
           </span>
