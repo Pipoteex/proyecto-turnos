25ba6b050f66d2f245c2ecc633ea83ad69cf785c
Martin <telaf1997@gmail.com>
2024-03-09 22:51:52 -0300

    add: primera pagina terminada

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['"Open Sans"', "sans-serif"],
+            },
+            colors: {
+                gray_header: "#F2F2F2",
+                blue_balanz: "#0D2055",
+                sky_blue_balanz: "#00A3E4",
+                gray_footer: "#DADADA",
+                gray_letter_footer: "#73757A",
+                gray_selector: "#D9D9D9",
+                messages: "#00A3E414",
+            },
+        },
     },
     plugins: [],
 };
